3023a02d02e964e59f3d45d7b8660025debf2137
toshiroto <chtopete@gmail.com>
2022-03-29 14:22:40 +0900

    Store Picker fixed

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,7 @@ import React from "react";
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store selector">
+      <form className="store-selector">
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store →</button>
