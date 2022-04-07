1245179fa3fbb9cb8d3c98494c5fccdc6ac92418
toshiroto <chtopete@gmail.com>
2022-04-07 19:20:24 +0900

    order state added

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,15 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes});
   };
+
+  addToOrder = (key) => {
+    // 1. Take a copy of State
+    const order = { ...this.state.order }
+    // 2. Add to the order or update the order
+    order[key] = order[key] + 1 || 1;
+    // 3. call state to update our state object
+    this.setState({ order });
+  }
   render () {
     console.log('fishes in render', this.state.fishes)
     return (
@@ -28,7 +37,14 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+                />
+            ))}
           </ul>
         </div>
         <Order />
