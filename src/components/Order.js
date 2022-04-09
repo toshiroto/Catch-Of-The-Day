95087b90fae76a87cf5cfa569ba6b14820eb4afe
toshiroto <chtopete@gmail.com>
2022-04-10 08:41:21 +0900

    order stated added

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = (key) => {
+    return <li>{key}</li>;
+  };
   render () {
+    const orderIds = Object.keys(this.props.order)
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + (count * fish.price);
+      }
+      return prevTotal;
+    }, 0);
+
     return (
-     <div className="order"> Order!!</div>
+     <div className="order-wrap">
+     <h2>Order</h2>
+      <ul>
+        {orderIds.map(this.render.order)};
+      </ul>
+      <div className="total">
+        Total:
+        <strong>{formatPrice(total)}</strong>
+      </div>
+      <ul>
+       <li></li>
+      </ul>
+     </div>
     )
   }
 }
