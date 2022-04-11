0d3ff1a6743baa5219791ba3b0a9bc5bd075d7e8
toshiroto <chtopete@gmail.com>
2022-04-11 23:10:06 +0900

    live state

--- src/components/App.js
+++ src/components/App.js
@@ -44,6 +44,15 @@ class App extends React.Component {
     // 3. set the new fishes object to state
     this.setState({ fishes });
   };
+
+  updateFish = (key, updatedFish) => {
+    // 1. Take a copy of current state
+    const fishes = {...this.state.fishes}
+    // 2. Update that state
+    fishes[key] = updatedFish;
+    // 3. set that to state
+    this.setState ({ fishes });
+  };
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes});
   };
@@ -76,6 +85,7 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}/>
       </div>
